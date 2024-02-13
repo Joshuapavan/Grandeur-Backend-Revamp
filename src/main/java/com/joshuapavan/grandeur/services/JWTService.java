5bce050b6bc24e4edb10ab34e854a5a86b5871a2
Joshua Pavan <joshuapavan35@gmail.com>
2024-02-13 18:02:17 +0530

    fixes jwt service

--- src/main/java/com/joshuapavan/grandeur/services/JWTService.java
+++ src/main/java/com/joshuapavan/grandeur/services/JWTService.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public interface JWTService {
     String generateToken(UserDetails userDetails);
-    String generateToken(Map<String,Object> extraClaims, UserDetails userDetails)
+    String generateToken(Map<String,Object> extraClaims, UserDetails userDetails);
     String getEmailFromToken(String token);
     boolean isTokenValid(String token, UserDetails userDetails);
 }
