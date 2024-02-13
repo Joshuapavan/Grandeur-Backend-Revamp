db5421b1382cf4592da0773c833c3baa0679f569
Joshua Pavan <joshuapavan35@gmail.com>
2024-02-13 17:55:11 +0530

    Adds another method for generating a token with extra claims, and adds encryption key for JWT.

--- src/main/java/com/joshuapavan/grandeur/services/JWTService.java
+++ src/main/java/com/joshuapavan/grandeur/services/JWTService.java
@@ -2,9 +2,11 @@ package com.joshuapavan.grandeur.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Map;
+
 public interface JWTService {
     String generateToken(UserDetails userDetails);
-
+    String generateToken(Map<String,Object> extraClaims, UserDetails userDetails)
     String getEmailFromToken(String token);
     boolean isTokenValid(String token, UserDetails userDetails);
 }
